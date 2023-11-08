1a56de4ccb0957ebaf78780aa0343e8642e9c4cd
Mario Bojos <mariob@codev.com>
2023-11-08 12:33:46 +0800

    Initial commit. Running code. Passed pint and phpstan.

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import {ref} from 'vue';
+import {Link} from '@inertiajs/vue3';
+
+const greeting = ref('Welcome to About page');
+
+</script>
+
+<template>
+    <h1>{{ greeting }}</h1>
+    <Link href="/">Go to Welcome page</Link>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import {ref} from 'vue';
+import {Link} from '@inertiajs/vue3';
+
+const greeting = ref('Welcome to  Your Laravel + Vue.js + TypeScript App!');
+
+</script>
+
+<template>
+    <h1>{{ greeting }}</h1>
+    <Link href="/about">Go to About page</Link>
+</template>
+
+<style scoped>
+
+</style>
